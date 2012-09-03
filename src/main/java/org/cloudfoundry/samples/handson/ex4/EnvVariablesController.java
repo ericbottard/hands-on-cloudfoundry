e74348c773db74f0724c108aaf47e8324b15fd5b
eric.bottard <eric.bottard@gmail.com>
2012-09-03 22:02:47 +0200

    Added instructions as a comment

--- src/main/java/org/cloudfoundry/samples/handson/ex4/EnvVariablesController.java
+++ src/main/java/org/cloudfoundry/samples/handson/ex4/EnvVariablesController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EnvVariablesController {
 
+	/**
+	 * Use the out parameter to dump the contents of the environment variables.
+	 * 
+	 * Notice how CloudFoundry instructs the app about services it provides
+	 * inside variables named {@code VCAP_something}.
+	 */
 	@RequestMapping("/ex4")
 	public void dump(PrintWriter out) throws Exception {
-		//...
+		// ...
 	}
-	
-	
+
 }
