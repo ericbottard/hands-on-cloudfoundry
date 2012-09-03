5efe800e43a4be6090d83b735eec3bd49d99ae55
eric.bottard <eric.bottard@gmail.com>
2012-09-03 23:10:18 +0200

    Drafted ex5.

--- src/main/java/org/cloudfoundry/samples/handson/config/AppConfig.java
+++ src/main/java/org/cloudfoundry/samples/handson/config/AppConfig.java
@@ -1,13 +1,40 @@
 package org.cloudfoundry.samples.handson.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(value="org.cloudfoundry.samples.handson", excludeFilters={@Filter(Configuration.class)})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
+	/**
+	 * Allows controllers to just return a String as a view name, that will be
+	 * resolved as a jsp page inside /WEB-ING/views. 
+	 */
+	@Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setViewClass(JstlView.class);
+        internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+	
+	/**
+	 * Allows mapping the {@link DispatcherServlet} to "/" while still being able to serve
+	 * static resources.
+	 */
+	@Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
